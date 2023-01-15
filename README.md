a8acddb98028c860ddd38190f80813cc4dcb9399
angel Escamilla <angel4esc@gmail.com>
2023-01-15 12:24:32 -0700

    Created the template, Just need to make it work now.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Week9Challenge
+# undefined
+## Table of Contents
+ - [Description](#Description)
+ - [License](#License)
+ - [Questions](#Questions)
+## Description
+ undefined
+## License
+ undefined
+## Questions
+ If you have any questions for me here is my github account link undefined also this is my email undefined. Feel free to reach out.
---
+++ index.js
@@ -0,0 +1,59 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your application?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Give me a brief description of what your application does.",
+        name: "description"
+    },
+    {
+        type: "checkbox",
+        message: "Pick a license for your website",
+        choices: ["Big license","little license","right size license"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Copy your github account link please.",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Give me your email please.",
+        name: "email"
+    }
+];
+
+inquirer
+ .prompt(questions)
+  .then(answers => {
+    console.log(`So it's ${answers.snapchat} and ${answers.email}`);
+    createReadMe(questions);
+});
+
+const createReadMe = data => {
+    console.log(data)
+    const template = `# ${data.title}
+## Table of Contents
+ - [Description](#Description)
+ - [License](#License)
+ - [Questions](#Questions)
+## Description
+ ${data.description}
+## License
+ ${data.license}
+## Questions
+ If you have any questions for me here is my github account link ${data.github} also this is my email ${data.email}. Feel free to reach out.
+`
+    console.log(template);
+    fs.writeFile(`README.md`, template, err => {
+        err ? console.log(error) : console.log("Success!")
+    });
+} 
